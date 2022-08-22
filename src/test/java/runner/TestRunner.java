13948fa862f0e6bfa90fb0204744238ae594c734
GinoMario <gino.jimenez@globant.com>
2022-08-22 10:06:27 -0500

    change tag

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = ("steps"),
-        tags =("@mregr")
+        tags =("@smokeTest")
 )
 
 public class TestRunner {
